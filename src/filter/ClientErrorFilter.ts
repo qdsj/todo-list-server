a5dde076a6a5758cac1ec7f4684665601fc54139
zenoskongfu <zenos.chen@foxmail.com>
2024-12-20 16:03:40 +0800

    update code

--- src/filter/ClientErrorFilter.ts
+++ src/filter/ClientErrorFilter.ts
@@ -1,21 +1,15 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import ClientErrorException from 'src/exception/clientErrorException';
+import ClientErrorException from 'src/exception/ClientErrorException';
 
 @Catch(ClientErrorException)
 export class ClientErrorFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
+  catch(exception: ClientErrorException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    // const request = ctx.getRequest();
 
-    if (exception instanceof ClientErrorException) {
-      response.status(400).json({
-        statusCode: 400,
-        message: exception.message,
-      });
-    } else {
-      // 如果不是 ClientErrorException，抛出原始错误
-      // super.catch(exception, host);
-    }
+    response.status(400).json({
+      statusCode: 400,
+      message: exception.message,
+    });
   }
 }
